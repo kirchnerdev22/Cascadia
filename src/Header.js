3188a73be099d305388a34973020c14fdc0eaf79
kirchnerdev22 <trentonlkirchner@gmail.com>
2023-09-18 23:32:42 -0500

    We are starting to get some style in. There is an image on the homepaged and some styling for the Header

--- src/Header.js
+++ src/Header.js
@@ -6,53 +6,61 @@ import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
-    <header className="App-header">
-      <div className="left-links">
-        <div className="header-link">
-        <Link to="/" className="link-title">
-          Home
-        </Link>
-        </div>
-        <div className="header-link">
-          <Link to="/shop" className='link-title'>Shop</Link>
-          <div className="dropdown-content">
-            <Link to="/Coffee" className='link-title'>Coffee</Link>
-            <Link to="/equipment" className='link-title'>Brewing Equipment</Link>
-            <Link to="/merch" className='link-title'>Merch</Link>
-          </div>
-        </div>
-        <div className="header-link">
-          <Link to="/learn" className='link-title'>Learn</Link>
-          <div className="dropdown-content">
-            <Link to="/about" className='link-title'>About Us</Link>
-            <Link to="/Guides" className='link-title'>Brewing Guides</Link>
-            <Link to="/classes" className='link-title'>Coffee Classes</Link>
-            <Link to="/contact" className='link-title'>Contact Us</Link>
-          </div>
-        </div>
-        <div className="header-link">
-          <Link to="/locations" className='link-title'>Locations</Link>
-          <div className="dropdown-content">
-            <Link to="/Basecamp" className='link-title'>Basecamp</Link>
-            <Link to="/thewharf" className='link-title'>The Wharf</Link>
-          </div>
-        </div>
+    <header >
+      {/* Announcement Bar */}
+      <div className="announcement-bar">
+        <p className="announcement-text">
+          Spend $45+ and we'll automatically apply free shipping to your order. Shop away!
+        </p>
       </div>
-
-      <h1 className="name-title">
-        <Link to="/" className="main-title-link">
-          Cascadia
-        </Link>
-      </h1>
-
-      <div className="right-links">
-        <div className="header-link">
-        <Link to="/account" className='link-title'>Account</Link>
+      <div className="App-header">
+        <div className="left-links">
+          <div className="header-link">
+            <Link to="/" className="link-title">
+              Home
+            </Link>
+          </div>
+          <div className="header-link">
+            <Link to="/shop" className='link-title'>Shop</Link>
+            <div className="dropdown-content">
+              <Link to="/Coffee" className='link-title'>Coffee</Link>
+              <Link to="/equipment" className='link-title'>Brewing Equipment</Link>
+              <Link to="/merch" className='link-title'>Merch</Link>
+            </div>
+          </div>
+          <div className="header-link">
+            <Link to="/learn" className='link-title'>Learn</Link>
+            <div className="dropdown-content">
+              <Link to="/about" className='link-title'>About Us</Link>
+              <Link to="/Guides" className='link-title'>Brewing Guides</Link>
+              <Link to="/classes" className='link-title'>Coffee Classes</Link>
+              <Link to="/contact" className='link-title'>Contact Us</Link>
+            </div>
+          </div>
+          <div className="header-link">
+            <Link to="/locations" className='link-title'>Locations</Link>
+            <div className="dropdown-content">
+              <Link to="/Basecamp" className='link-title'>Basecamp</Link>
+              <Link to="/thewharf" className='link-title'>The Wharf</Link>
+            </div>
+          </div>
         </div>
-        <div className='header-link'>
-        <Link to="/cart" className="link-title">
-            <FontAwesomeIcon icon={faShoppingCart} /> Cart {/* Shopping Cart Icon */}
+
+        <h1 className="name-title">
+          <Link to="/" className="main-title-link">
+            Cascadia
           </Link>
+        </h1>
+
+        <div className="right-links">
+          <div className="header-link">
+            <Link to="/account" className='link-title'>Account</Link>
+          </div>
+          <div className='header-link'>
+            <Link to="/cart" className="link-title">
+              <FontAwesomeIcon icon={faShoppingCart} /> Cart {/* Shopping Cart Icon */}
+            </Link>
+          </div>
         </div>
       </div>
     </header>
--- src/Homepage.js
+++ src/Homepage.js
@@ -1,57 +1,20 @@
 import React from 'react';
 import './Homepage.css'; // Import your external CSS file
-
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom (assuming you're using React Router)
 
 function HomePage() {
   return (
     <div className="home-page">
-      
-      
-      <header className="hero">
-        <h1>Welcome to Cascadia Coffee Roasters</h1>
-        <p>Your Perfect Coffee Experience in the Heart of the Pacific Northwest</p>
-      </header>
+      <h1 className="main-title">The Coast is calling.</h1>
+      <p className="sub-title">Roasted locally since 2018.</p>
 
-      <section className="about-section">
-        <h2>About Us</h2>
-        <p>
-          Nestled in the picturesque town on the Washington Coast, Cascadia Coffee Roasters brings you the finest coffee
-          beans from around the world. With a backdrop of Olympic National Park and the majestic Mt. Rainier in view,
-          our cozy cafe is the perfect place to relax and savor the beauty of the Pacific Northwest.
-        </p>
-        <p>
-          Located just 1.5 hours from Seattle, our small mountain-side town offers a tranquil escape from the hustle and
-          bustle of the city, making it an ideal destination for coffee enthusiasts and nature lovers alike.
-        </p>
-      </section>
-
-      <section className="menu-section">
-        <h2>Our Menu</h2>
-        <p>Explore our diverse menu of handcrafted coffee, artisanal pastries, and more. Whether you're a coffee aficionado or simply looking for a delightful treat, we have something for everyone.</p>
-        {/* You can add menu items and descriptions here */}
-      </section>
-
-      <section className="visit-section">
-        <h2>Visit Us</h2>
-        <p>Plan your visit to Cascadia Coffee Roasters and immerse yourself in the beauty of the Pacific Northwest. Experience the aroma of freshly roasted coffee beans and the warmth of our welcoming cafe.</p>
-        <p>We are located at:</p>
-        <address>
-          123 Coffee Lane<br />
-          Mountainside Town, WA 98765<br />
-        </address>
-        <p>Hours of Operation:</p>
-        <p>Monday - Friday: 7:00 AM - 6:00 PM</p>
-        <p>Saturday - Sunday: 8:00 AM - 5:00 PM</p>
-      </section>
-
-      <section className="instagram-section">
-        <h2>Follow Us on Instagram</h2>
-        {/* You can add an Instagram feed or links here */}
-      </section>
-
-      
+      <div className="navigation-links">
+        <Link to="/shop">Shop</Link>
+        <Link to="/locations">Locations</Link>
+      </div>
     </div>
   );
 }
 
 export default HomePage;
+
